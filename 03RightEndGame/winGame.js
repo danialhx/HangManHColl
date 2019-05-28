e45ce36e744fc13938f98bc47cca472c9a7ad17f
danialhx <hussin.danial@gmail.com>
2019-05-28 12:32:28 +0800

    Create winGame.js
    moved justin's file from root to folder

---
+++ 03RightEndGame/winGame.js
@@ -0,0 +1,14 @@
+/**
+ * winGame
+ *
+ * Checks if the word the word is filled with letters and prings 'you win game'
+ *
+ * @param {array}   answerArray           Description.
+ * @param {array}   trueArray        Description of optional variable.
+ * 
+ * @return {return} 
+ */
+
+winGame function(input){
+    console.log(input); 
+}
