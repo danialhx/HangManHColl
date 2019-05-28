4d088400c886f340763787e881e4a40c0547831a
mazecoder <46983666+mazecoder@users.noreply.github.com>
2019-05-28 13:42:27 +0800

    Add files via upload

--- 04WrongEndGame/ray.js
+++ 04WrongEndGame/ray.js
@@ -1,7 +1,21 @@
+/* 
+ Pick a random word.
+ Take the player’s guess.
+ Check that the player’s guess is a valid letter.
+ Keep track of letters the player has guessed.
+ Show the player their progress.
+ Finish when the player has guessed the word.
+*/
 
-for (var k = 0; k < word.length; k++) {
-    if (word[k] === inputAnswer) {
-        expectedAnswer[k] = inputAnswer;
-        remainingLetters--;
-     }
-}
+let limit = 9;
+let wrongGuesses;
+
+
+
+    let remainingGuesses = limit - wrongGuesses; 
+
+    if ( expectedAnswer.indexOf( inputAnswer ) === -1 ) {
+        console.log( "That letter in not in the secret word." );
+        wrongGuesses += 1;
+        return;
+    }
