fde906a4c2cfc9cd055451ef33b889264e5bd6dd
ian Lim <ian.lim.tn@gmail.com>
2019-05-28 16:31:38 +0800

    added list-choices.js and added jquery for list-choices.js

---
+++ LastFolder/list-choices.js
@@ -0,0 +1,28 @@
+// Allow an array to save all the right characters chosen made
+
+//Accept input from user
+let input = "a"; // Sample input
+
+// Initialize Array to store correct words
+let correctArr = []
+
+// Initialize Array to store wrong words
+let wrongArr = []
+
+//function to store wrong words in a globally accessible array 
+// to be placed in the comparision logic that is run when word character is wrong
+
+
+// Allow an array to save all the wrong characters chosen
+function wrongChar (input) {
+    wrongArr.unshift(`${input}`)
+    console.log(`The letter ${input} has been added to wrongArr`) 
+};
+
+
+wrongChar(input); // test whether code is working
+
+console.log(wrongArr); // Expected output = ['a']
+
+
+
