90d52a7a12204580b036803626768d7691efd828
Justin <justinthong93@gmail.com>
2019-05-28 12:16:17 +0800

    justin files added

---
+++ printRight.js
@@ -0,0 +1,17 @@
+/**
+* printRight
+*
+* takes correct letter and passes it to front end
+*
+* @param {bool}   bool     
+* @param {string}  inputLetter    
+* 
+* @return {return} 
+*/
+
+function printRight(bool, inputLetter){
+    
+    
+    $( "#rightLetter" ).append( $( "h2" ) );
+    
+}
---
+++ winGame.js
@@ -0,0 +1,15 @@
+
+/**
+ * winGame
+ *
+ * Checks if the word the word is filled with letters and prings 'you win game'
+ *
+ * @param {array}   answerArray           Description.
+ * @param {array}   trueArray        Description of optional variable.
+ * 
+ * @return {return} 
+ */
+
+winGame function(input){
+    console.log(input); 
+}
